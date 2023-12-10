8da2d48d33b98835626e09ae09023f7ad1128524
THE LUJAIN <silversocer17@gmail.com>
2023-12-10 18:25:09 +0300

    10th Dec testing

---
+++ iterationmethedtasks/arrows.js
@@ -0,0 +1,35 @@
+// function sum (x,y){
+//     return x+y;
+// }
+// //let arrofun = (x,y) => x+y;
+// const nums = [1,2,3,4,5];
+// //nums.map(nums.map)
+
+// function sum(x) => {
+//     return x + 3
+// }
+
+function fn1(){
+
+}
+const fn1 = ()=>{
+    console.log(x);
+    return x;
+
+}
+function fn3(x,y,z){
+    const sum = x+y+z;
+    return sum;
+}
+const fn3 = (x,y,z)=>{
+    const sum = x+y+z;
+    return sum;
+    function fn4(x){
+        const sqr = x * x;
+        return sqr;
+    }
+};
+const fn5 = (x) {
+    return x+x;
+}
+const fn5 = x => x+x;
---
+++ objects.js
@@ -0,0 +1,108 @@
+// const car = {
+//     color:'black',
+//     model:'2003',
+//     manufacturer:'ford'
+// }
+// console.log(car.model);
+// 000000000000000000000000000000000000
+
+
+// const order = {
+//     product:"Latte",
+//     cost:1.2
+//     quantity:2
+// }
+// console.log(order.cost * order.quantity);
+//000000000000000000000000000000000000000000
+
+
+// const person ={
+//     name: "Abdulwahhab",
+//     age: 24,
+//     hobbies: ["Basketball","Hiking"],
+// };
+// person.degree = "Bachelor";
+// //this will overwright the old age
+// person.age = 25;
+// person.hobbies.push("Fishing");
+// console.log(person);
+// console.log(person.hobbies);
+//00000000000000000000000000000000000000000000000000
+// const languages = {
+//     web:[
+//         "javascript"," python"
+//     ],
+// };
+// languages web.push(Swift)
+// 0000000000000000000000000000000000000000000
+// const storage = {
+//     drinks: {
+//         cold:['Flash'],
+//         hot:['Latte']
+//     }
+// }
+// // console.log(storage.drinks.cold)
+// // storage.drinks.cold.push('Kinza')
+// // storage.drinks.roomTempreture = [30];
+// // storage.food = {};
+// console.log(storage);
+
+// 00000000000000000000000000000000000000000000
+
+//const watches = ["Apples","Rolex"]
+
+// const watches = [
+//     {
+//         name:"Apple",
+//         cost:130
+//     },
+//     {
+//         name:"Rolex",
+//         cost:3200
+//     }
+// ]
+// console.log(watches[0.]);
+//00000000000000000000000000000000000000000000
+// const employees = [
+//     {
+//     position: "Manager",
+//     salary: 4300
+//     },
+//     {
+//         position:"CTO",
+//         salary: 1000
+//     }
+// ]
+// //console.log(employees[1])
+// employees[0].company = "Microsoft";
+// employees[1].company = "OenAI";
+//000000000000000000000000000000000000000000
+// const store = [
+//     {
+//         name:"Tshirt",
+//         cost:12,
+//         color:"Purple",
+//     },{
+//         name:"Pants",
+//         cost:16,
+//         color:"Beige",
+//         ordered:0
+//     }
+// ]
+// store[0].ordered = 2;
+// store[1].ordered = 3;
+// const total = store[0].ordered +
+// cost*store[0].ordered+store[1].ordered;
+
+// console.log(total);
+//0000000000000000000000000000000000000000000000
+const package = {
+    color: "red",
+    order:0,
+    findeFishingSpot(){
+        console.log("Gooo JNOOOOB");
+    },
+};
+package.findeFishingSpot();
+console.log()
+
